39db724877e7bf685961395f823f147197286ca6
HugoPVoodoo <hugo.peyron@voodoo.io>
2025-03-13 16:33:24 +0100

    Initial Commit

---
+++ ReadWriteCount/ReadWriteCountApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadWriteCountApp.swift
+//  ReadWriteCount
+//
+//  Created by Hugo Peyron on 13/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct ReadWriteCountApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
